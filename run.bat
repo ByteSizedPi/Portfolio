ca6b23d2896a6a2c084f3219f053d58915b39dfc
Johan Venter <johann12venter@gmail.com>
2021-06-25 11:15:39 +0200

    mobile friendly

--- run.bat
+++ run.bat
@@ -1,4 +1,2 @@
-cd C:\Users\31656072\Desktop\Personal\1 Coding\Angular\Portfolio\portfolio
-git add .
-git commit -m ""
+cd C:\Users\31656072\Desktop\Personal\1 Coding\Angular\Portfolio\portfolio\docs
 git push
